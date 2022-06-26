c0d5e9228499e0dd3ab36fd14e10d7bca30bc621
Romar Cablao <romarcablao@gmail.com>
2022-06-26 19:54:46 +0800

    Update validation

--- kubernetes/01-namespaces-and-pods/step6/verify.sh
+++ kubernetes/01-namespaces-and-pods/step6/verify.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-if kubectl get ns | grep cloudnative; then exit 1; fi
-if kubectl -n cloudnative get pod --selector=app=jenkins --selector=company=sp; then exit 1; fi
-if kubectl -n cloudnative get pod jenkins -oyaml | grep "image: jenkins/jenkins:lts"; then exit 1; fi
+validate_k8s_ns=$(kubectl get ns | grep cloudnative | wc -l)
+validate_k8s_pod=$(kubectl -n cloudnative get pod --selector=app=jenkins --selector=company=sp --no-headers | wc -l)
+validate_k8s_pod_label=$(kubectl -n cloudnative get pod jenkins -oyaml | grep "image: jenkins/jenkins:lts" | wc -l)
+
+if [ $validate_k8s_ns -le 0 ]; then
+  echo "Hint: Make sure have the required namespace created.."
+fi
+
+if [ $validate_k8s_pod -le 0 ]; then
+  echo "Hint: Try checking where your pod is running..."
+fi
+
+if [ $validate_k8s_pod_label -le 0 ]; then
+  echo "Hint: Hmmm... how 'bout the labels?"
+fi

7f0270592802a2cae9062e3b754d2a48453ca98f
Romar Cablao <romarcablao@gmail.com>
2022-06-26 23:29:48 +0800

    Uppdate

--- kubernetes/04-advanced-deployment/startup.sh
+++ kubernetes/04-advanced-deployment/startup.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env sh
-echo "$(date) copy to old" > ~/.log.txt
-mkdir /old
-cp -r /root/resources/* /old/
-rm -rf /root/resources
-
-echo "$(date) copy to new" >> ~/.log.txt
 mkdir -p /root/resources
-cp -r /root/newresources/* /root/resources/
-rm -rf /root/newresources
-
-echo "$(date) delete" >> ~/.log.txt
-kubectl delete pods --all
-kubectl delete deployments --all
-kubectl delete services --all
-
-echo "$(date) done" >> ~/.log.txt

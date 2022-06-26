84e27b9e0f119a682577b811324ce96b7c917659
Romar Cablao <romarcablao@gmail.com>
2022-06-26 21:39:56 +0800

    Update

--- kubernetes/09-volumes/step1-verify.sh
+++ kubernetes/09-volumes/step1-verify.sh
@@ -2,9 +2,9 @@
 have=$(kubectl exec nginx -- ls /data/nginx/)
 want="test.txt"
 
-if [ "$have" = "$want" ]; then
+if [ "$have" != "$want" ]; then
   echo "done"
-  exit 0
+  exit 1
 fi
 
 echo "You should have ${have} you have ${want}"

3ed2fa9a1cdf63b45d201e0abd03d1a4daea2873
Romar Cablao <romarcablao@gmail.com>
2022-06-27 12:16:12 +0800

    Update

--- docker/02-docker-network/step1-verify.sh
+++ docker/02-docker-network/step1-verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-validate_docker_delete=$(docker ps | grep centos3 | wc -l)
+validate_docker_delete=$(docker ps -a | grep centos3 | wc -l)
 
 if [ $validate_docker_delete -ge 1 ]; then
   exit 1

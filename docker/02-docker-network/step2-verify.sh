6fc676d45c78a6137ae14837031857f084fa426f
Romar Cablao <romarcablao@gmail.com>
2022-06-27 12:22:27 +0800

    Update

--- docker/02-docker-network/step2-verify.sh
+++ docker/02-docker-network/step2-verify.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+LOGFILE=/step1-verify.log
+#set -e # exit once any command fails
+{
+    set -e
+    date
+    docker container run -it --network my_bridge centos ping centos3 -c 3
+} >> ${LOGFILE} 2>&1
 
-validate_docker_ping=$(docker container run -it --network my_bridge centos ping centos3 -c 3 | grep "3 packets transmitted" | wc -l)
-
-sleep 10
-if [ $validate_docker_ping -le 0 ]; then
-  exit 1
-fi
+echo "done"
